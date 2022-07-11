11336ebfe79a7780f638263d303076255f5c11e9
svvchnko <alex33savchenko@gmail.com>
2022-07-11 14:51:24 +0300

    js carousel is back

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop: true,
-        // autoplay: true,
-        // autoplayTimeout: 3000,
+        autoplay: true,
+        autoplayTimeout: 3000,
         margin:0,
         nav:true,
         navText: [ '', ' ' ],
